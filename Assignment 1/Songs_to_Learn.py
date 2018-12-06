3996b89d79d5b13596496a16c83259c0233022da
MicWik <michaelwidjaja.wiko@my.jcu.edu.au>
2018-12-06 15:17:27 +0800

    2.0

--- Assignment 1/Songs_to_Learn.py
+++ Assignment 1/Songs_to_Learn.py
@@ -1,9 +1,13 @@
+import csv
+
 def name(name):
     name = input("Please enter your name : ")
     print("Hi " + name + "!" + " Welcome back.")
     return name
 
 def menu():
+    data = new_song + learned_song
+    data.sort()
     print("|--------------------------------------|")
     print("| Songs to Learn 1.0 - by Michael Wiko |")
     print("|--------------------------------------|")
@@ -14,12 +18,8 @@ def menu():
         print("Invalid input")
         entry = input(">>> ")
     if entry == "L" or entry == "l" :
-        print("{}.{} {:35} -  {:32} ({:4})".format(0, "*", "Heartbreak Hotel", "Elvis Presley", 1956))
-        print("{}.{} {:35} -  {:32} ({:4})".format(1, " ", "Somebody That I Used to Know", "Gotye featuring Kimbra", 2012))
-        print("{}.{} {:35} -  {:32} ({:4})".format(2, "*", "Macarena", "Los Del Rio", 1996))
-        print("{}.{} {:35} -  {:32} ({:4})".format(3, "*", "I Want To Hold Your Hand", "The Beatles", 1964))
-        print("{}.{} {:35} -  {:32} ({:4})".format(4, " ", "Boom Boom Pow", "The Black Eyed Peas", 2009))
-        print("{}.{} {:35} -  {:32} ({:4})".format(5, "*", "My Sharona", "The Knack", 1979))
+        print("List of songs ( * = new songs ):")
+        song_list(data, new_song, learned_song)
     elif entry == "A" or entry == "a" :
         print("Can't add anything.")
     elif entry == "C" or entry == "c" :
@@ -27,6 +27,32 @@ def menu():
     elif entry == "Q" or entry == "q" :
         print("Bye " + main_name + " ! See you soon.")
 
+def become_list():
+    input_file = open("songs.csv", "r")
+    number = 0
+    for line in input_file:
+        song=line.strip().split(",")
+        if song[3] == "n":
+            song = [number, song[0], song[1], song[2]]
+            new_song.append(song)
+        elif song[3] == "y":
+            song = [number, song[0], song[1], song[2]]
+            learned_song.append(song)
+        number += 1
+    input_file.close()
+    print("{} items loaded from songs.csv".format(len(new_song + learned_song)))
 
+def song_list(data, new_song, learned_song):
+    for each in data:
+        if each in new_song:
+            print("{}.{} {:35} -  {:32} ({:4})".format(char[0], "*", char[1], char[2], char[3]))
+        elif each in learned_song:
+            print("{}.{} {:35} -  {:32} ({:4})".format(char[0], " ", char[1], char[2], char[3]))
+
+
+
+new_song = []
+learned_song = []
+data = []
 main_name = name(name)
 menu()
